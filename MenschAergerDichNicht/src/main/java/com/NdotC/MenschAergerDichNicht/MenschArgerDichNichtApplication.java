40618b60b2ad6a5b0d929863fd250cdfe14a811b
NdotC <niklasschmick@gmail.com>
2024-06-18 11:14:08 +0200

    Spring init

---
+++ MenschAergerDichNicht/src/main/java/com/NdotC/MenschAergerDichNicht/MenschArgerDichNichtApplication.java
@@ -0,0 +1,13 @@
+package com.NdotC.MenschAergerDichNicht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenschArgerDichNichtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenschArgerDichNichtApplication.class, args);
+	}
+
+}
---
+++ MenschAergerDichNicht/src/test/java/com/NdotC/MenschAergerDichNicht/MenschArgerDichNichtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NdotC.MenschAergerDichNicht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenschArgerDichNichtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
